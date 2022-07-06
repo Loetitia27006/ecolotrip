ac0d75f2d931eba9b07576ebefad77ec70cb7ca9
loetitia <audiger.loetitia@gmail.com>
2022-07-06 08:35:55 +0200

    commencement

---
+++ connexion.php
@@ -0,0 +1,34 @@
+<?php include_once './includes/header.php'; 
+
+$title = 'ECOLOTRIP'
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title; ?></title>
+    <link rel="stylesheet" href="style/connexion.css">
+</head>
+<body>
+    <h1>Bon retour triper !</h1>
+    <div class="form">
+        <form action="" method="post">
+            
+            <p><input type="email" name="email" id="" placeholder="ton adresse mail"></p>
+        
+            </p><input type="password" name="password" id="" placeholder="ton mot de passe"></p>
+            <p class="connection"><button type="submit">  Se connecter</button></p>
+            <div class="inscription">
+                <a href="#">mot de passe oublié</a>
+                <p >pas de compte<a href="#">s'inscrire</a></p>
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <link rel="stylesheet" href="./style/style.css">
+    <nav>
+        <div class="burger"></div>
+       <div class="logo">
+            <img src="./image/image.jpg" alt="">
+       </div>
+       <div class="basket"></div> 
+    </nav>
+</header>
